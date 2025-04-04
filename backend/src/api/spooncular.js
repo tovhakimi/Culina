489d4782b4b9b85e815246bf453e00f8a98d4154
tovhakimi <hakimitov@gmail.com>
2025-04-04 15:03:53 +0300

    .env init

--- backend/src/api/spooncular.js
+++ backend/src/api/spooncular.js
@@ -1,12 +1,15 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+
 const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-const API_KEY = '8037d57d69ea46e18bdc8366605b7461';
+const API_KEY = process.env.SPOONCULAR_API_KEY;
 
 router.get('/products', async (req, res) => {
     const query = req.query.query;
     const number = req.query.number;
+    console.log('Loaded API Key:', API_KEY);
 
     if (!query){
         return res.status(400).json({message: "Query parameter is required!"});
@@ -15,9 +18,9 @@ router.get('/products', async (req, res) => {
         const url = `https://api.spoonacular.com/food/products/search?query=${encodeURIComponent(query)}&number=${number}&apiKey=${API_KEY}`;
         const response = await axios.get(url);
 
-        if (!response.data.products || s.length === 0) {response.data.product
+        if (!response.data.products || response.data.products.length === 0) {
             return res.status(404).json({ message: "No products found." });
-        }
+        }        
 
         const products = response.data.products.map(p => ({
             name: p.title,
