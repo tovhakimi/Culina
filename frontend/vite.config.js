6b15d4b2725e08da7bad61f26f6e0ef8eb3b9247
tovhakimi <hakimitov@gmail.com>
2025-04-04 17:43:28 +0300

    Proxy init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/personal-menu': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/api/spooncular': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    },
+  },
+});
